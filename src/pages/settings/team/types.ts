3c61101068d1fc8229fc6793eebcb6d3db5d7076
AnslemCode <102703239+AnslemCode@users.noreply.github.com>
2024-11-06 09:34:01 +0100

    fix: types import fix

--- src/pages/settings/team/types.ts
+++ src/pages/settings/team/types.ts
@@ -4,5 +4,5 @@ export type Team = {
   email: string
   role: string
   twoFactorAuthEnabled: boolean
-  dateJoined: string // ISO date format (e.g., '2023-10-21T14:48:00.000Z')
+  dateJoined: string
 }
